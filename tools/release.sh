912cd756d491010a898a736d8ef53c339ab98a91
chau.vinh.tuan <chau.vinh.tuan@linecorp.com>
2021-09-05 19:55:53 +0900

    Update release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,10 +1,21 @@
+# ./release.sh <path-to-src> <commit comment>
 src=$1
 git config user.email "tunachauict@gmail.com"
 git config user.name "Tuan Chau"
 
-mv $src/main.css .
-mv $src/MonoSketch.js .
-mv $src/MonoSketch.js.map .
+hash=$(git rev-parse --short HEAD)
+dest="v1-$hash"
+
+rm */app.js
+rm */app.js.map
+rm */app.css
+
+mkdir $dest
+mv $src/main.css ./$dest/app.css
+mv $src/MonoSketch.js ./$dest/app.js
+mv $src/MonoSketch.js.map ./$dest/app.js.map
+
+sed -e  "s/scriptdir/$dest/g" template_index.html > index.html
 
 git add -A
 git commit -m "Update from $2"
